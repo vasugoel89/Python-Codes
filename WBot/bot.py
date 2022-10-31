93b3bdff3ede3d8c3fe80b11e509c05dc6c1b0d0
Vasu Goel <vasugoel89@gmail.com>
2022-10-31 21:26:49 +0530

    Add files via upload

---
+++ WBot/bot.py
@@ -0,0 +1,11 @@
+from calendar import day_abbr
+from ssl import OPENSSL_VERSION_NUMBER
+import pyautogui as auto
+import time
+
+while True:
+    auto.write("Hello there !")
+    time.sleep(1)
+    auto.write(" I am Python WBot.")
+    auto.press('enter')
+    
